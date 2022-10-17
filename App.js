9fdbecb96e5ba696b231cad780f6f5c308425e64
Joseph Kim <kimg009263@gmail.com>
2022-10-18 12:01:55 +1300

    push to master

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 // app.js
-
+require("dotenv").config({ path: ".env"});
+const path = require('path');
 const express = require('express');
 const connectDB = require('./config/db')
 var cors = require('cors');
@@ -10,6 +11,18 @@ connectDB();
 
 // cors
 app.use(cors({ origin: true, credentials: true }));
+
+/*if(process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, '/client/build')));
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
+    })
+} else {
+    app.get('/', (req, res) => {
+        res.send("Api running");
+    })
+}*/
 
 app.get('/', (req, res) => res.send('Hello world!'));
 
